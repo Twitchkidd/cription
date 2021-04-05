8d8392ae1c671fc5a4383d56924a352fea6c3c5e
Gareth Field <field.gareth@gmail.com>
2021-04-05 05:38:14 -0400

    progress! next step ui, then opening the file!

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // ? Cription: Transcription Tool
 
 /*
- *  Okay MINIMUM (because I know you can imagine a web app for this, mais ...)
- *  ... Quicktime has controls from applescript, which can be run from the CL ...
- *
+ * Okay MINIMUM (because I know you can imagine a web app for this, mais ...)
+ * ... Quicktime has controls from applescript, which can be run from the CL ...
+ * `brew install youtube-dl` ... Okay, well, now we know, if you run:
+ * `youtube-dl [RSS FEED]` it will download them all starting from the most
+ * recent, to present working directory. Okay, `youtube-dl -j --flat-playlist`
+ * returns like object, newline, object, ai., so it just needs to be wrapped
+ * in something and commas added, anyway, then you get data including url, title,
+ * episode_number, all sorts of good stuff. I imagine ideally you could either pass
+ * no arguments and it would give you a list starting with the most recent, or you
+ * could pass an episode number to hit the ground running.
  */
